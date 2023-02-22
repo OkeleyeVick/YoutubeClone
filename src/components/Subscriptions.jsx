4516ca5c992142623cf24d241d985bab26d1cde7
Vickkk <justusofnigeria@gmail.com>
2023-02-22 16:37:15 +0100

    using hooks

--- src/components/Subscriptions.jsx
+++ src/components/Subscriptions.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { useEffect, useMemo, useState } from "react";
 
 const Subscriptions = () => {
-	return <h1>Subscriptions</h1>;
+	const [youtubers, setYoutubers] = useState([]);
+
+	useEffect(() => {
+		const api_url = `https://randomuser.me/api/?results=2`;
+
+		const fetchUrl = async () => {
+			const response = await fetch(api_url);
+			const data = await response.json();
+			setYoutubers(data.results);
+		};
+
+		fetchUrl();
+	}, []);
+
+	const youtubersDatas = useMemo(() => {
+		return youtubers;
+	}, [youtubers]);
+
+	const fullname = (firstname, lastname) => {
+		return `${firstname} ${lastname}`;
+	};
+	return (
+		<h1>
+			{/* {youtubersDatas &&
+				youtubersDatas.map((youtuber, index) => {
+					const {
+						name: { first, last },
+						picture: { medium },
+					} = youtuber;
+					const f_name = fullname(first, last);
+
+				})} */}
+		</h1>
+	);
 };
 
 export default Subscriptions;
