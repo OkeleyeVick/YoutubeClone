cf853ed4b6079aac63e1d1f5f12e1dc888d84fde
Vickkk <justusofnigeria@gmail.com>
2023-02-25 03:23:53 +0100

    ...

--- src/components/DownloadVideos.jsx
+++ src/components/DownloadVideos.jsx
@@ -1,5 +1,5 @@
 import { Icon } from "@iconify/react";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useReducer, useRef } from "react";
 import {
 	Container,
 	ContentContainer,
@@ -37,21 +37,61 @@ import { Em } from "../assets/css/IndexPageStyles";
 import FakeImage from "../assets/images/netflix.webp";
 import { gridItems } from "./Objects";
 
+const extractAndCopyId = (youtubeLink) => {
+	const youtubeLinkId = youtubeLink.split(".be/")[1];
+	return youtubeLinkId;
+};
+
+const global = {
+	setError: "setError",
+	setData: "setData",
+	setId: "setYoutubeId",
+};
+
+const initialState = {
+	youtubeId: null,
+	data: {},
+	error: null,
+};
+
 const DownloadVideos = () => {
+	const { setError, setData, setId } = global;
+
 	const inputRef = useRef(null);
 
-	const [youtubeId, setYoutubeId] = useState("");
+	const reducerFunc = (state, action) => {
+		switch (action.type) {
+			case setData:
+				return {
+					...state,
+					data: action.data,
+				};
 
-	const extractAndCopyId = (youtubeLink) => {
-		const youtubeLinkId = youtubeLink.split(".be/")[1];
-		return youtubeLinkId;
+			case setError:
+				return {
+					...state,
+					data: {},
+					error: action.error,
+				};
+
+			case setId:
+				return {
+					...state,
+					youtubeId: action.id,
+				};
+
+			default:
+				throw Error("Cannot fetch data");
+		}
 	};
 
 	function handleSubmit(e) {
 		e.preventDefault();
 		const id = extractAndCopyId(inputRef.current.value);
-		setYoutubeId(id);
+		doAction({ type: setId, id });
 	}
+
+	const [currentState, doAction] = useReducer(reducerFunc, initialState);
 
 	useEffect(() => {
 		// https://ytstream-download-youtube-videos.p.rapidapi.com/dl?id=${youtubeIdRef.current} //!50 per day
@@ -67,17 +107,18 @@ const DownloadVideos = () => {
 			// signal: controller.signal,
 		};
 
-		const api_url = `https://youtube-media-downloader.p.rapidapi.com/v2/video/details?videoId=${youtubeId}`;
+		const api_url = `https://youtube-media-downloadWer.p.rapidapi.com/v2/video/details?videoId=${currentState.youtubeId}`;
 		const fetchVideos = async () => {
 			const response = await fetch(api_url, options);
+			if (!response.ok) doAction({ type: setError, error: response.status });
 			const data = await response.json();
-			console.log(data);
+			doAction({ type: setData, data });
 		};
 
 		fetchVideos();
 
 		// return () => controller.abort();
-	}, [youtubeId]);
+	}, [currentState.youtubeId, setData, setId, setError]);
 
 	return (
 		<PageContainer>
@@ -96,42 +137,38 @@ const DownloadVideos = () => {
 							</FormInputContainer>
 						</FormMainContainer>
 					</form>
-					{
-						<ResultContainer>
-							<ResultHeader>
-								<ResultImage>
-									<Image src={FakeImage} />
-								</ResultImage>
-								<ContentContainer>
-									<Title>
-										Will Smith's Life Advice Will Change You - One of the Greatest Speeches Ever | Will Smith Motivation
-									</Title>
-									<span>Duration: 0:10:45</span>
-									<span>Views: 4,080,048</span>
-								</ContentContainer>
-							</ResultHeader>
-							<MimeType>
-								<h3>Video</h3>
-							</MimeType>
-							<ResultTableHeader>
-								<HeadTitle>Quality</HeadTitle>
-								<HeadTitle>Type</HeadTitle>
-								<HeadTitle willChange>File size</HeadTitle>
-								<HeadTitle>Download</HeadTitle>
-							</ResultTableHeader>
-							<ResultTableBody>
-								<Quality>702p</Quality>
-								<Type>mp4</Type>
-								<FileSize willChange>-</FileSize>
-								<DownloadLink to="/">
-									<Em>
-										<Icon icon="ph:download-simple-light" />
-									</Em>
-									<span>Download</span>
-								</DownloadLink>
-							</ResultTableBody>
-						</ResultContainer>
-					}
+					<ResultContainer>
+						<ResultHeader>
+							<ResultImage>
+								<Image src={FakeImage} />
+							</ResultImage>
+							<ContentContainer>
+								<Title>Will Smith's Life Advice Will Change You - One of the Greatest Speeches Ever | Will Smith Motivation</Title>
+								<span>Duration: 0:10:45</span>
+								<span>Views: 4,080,048</span>
+							</ContentContainer>
+						</ResultHeader>
+						<MimeType>
+							<h3>Video</h3>
+						</MimeType>
+						<ResultTableHeader>
+							<HeadTitle>Quality</HeadTitle>
+							<HeadTitle>Type</HeadTitle>
+							<HeadTitle willChange>File size</HeadTitle>
+							<HeadTitle>Download</HeadTitle>
+						</ResultTableHeader>
+						<ResultTableBody>
+							<Quality>702p</Quality>
+							<Type>mp4</Type>
+							<FileSize willChange>-</FileSize>
+							<DownloadLink to="/">
+								<Em>
+									<Icon icon="ph:download-simple-light" />
+								</Em>
+								<span>Download</span>
+							</DownloadLink>
+						</ResultTableBody>
+					</ResultContainer>
 				</FormMainContainer>
 			</FormOuterContainer>
 			<Container>
